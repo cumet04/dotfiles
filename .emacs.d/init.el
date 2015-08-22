c344f232f1d6506fda4d0cd616ce31eebca1b450
medalhkr <medalhkr@gmail.com>
2015-08-23 02:10:23 +0900

    modify init.el for mac environment

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,25 +3,29 @@
 (load-theme 'molokai t)
 (enable-theme 'molokai)
 
-;; font
-(set-face-attribute 'default nil
-        :family "Menlo" ;; font
-        :height 160)    ;; font size
-(set-fontset-font
-        nil 'japanese-jisx0208
-        (font-spec :family "Hiragino Kaku Gothic ProN")) ;; font
-        ;; (font-spec :family "Ricty")) ;; font
-
 ;; display line-number left side
 (require 'linum)
 (global-linum-mode)
 (setq linum-format "%3d ")
 
+;; for window system
+(if window-system 
+    (progn
+        (set-frame-parameter nil 'alpha 95)
+        ;; Ricty フォントの利用
+        (create-fontset-from-ascii-font "Ricty-18:weight=normal:slant=normal" nil "ricty")
+        (set-fontset-font "fontset-ricty"
+                          'unicode
+                          (font-spec :family "Ricty" :size 18)
+                          nil
+                          'append)
+        (add-to-list 'default-frame-alist '(font . "fontset-ricty"))
+    ))
+
+
 ;; disable auto-indent
 (electric-indent-mode -1)
 
-;; hide menu bar
-(menu-bar-mode 0)
 (tool-bar-mode 0)
 
 (add-hook 'window-configuration-change-hook (lambda () (ruler-mode 1)))
@@ -41,42 +45,3 @@
                     (process-send-string proc text)
                     (process-send-eof proc)))
         )))
-
-;; ウィンドウリサイズ
-(define-key global-map (kbd "C-q") (make-sparse-keymap))
-(global-set-key (kbd "C-q C-r") 'window-resizer)
-
-(defun window-resizer ()
-  "Control window size and position."
-  (interactive)
-  (let ((window-obj (selected-window))
-        (current-width (window-width))
-        (current-height (window-height))
-        (dx (if (= (nth 0 (window-edges)) 0) 1
-              -1))
-        (dy (if (= (nth 1 (window-edges)) 0) 1
-              -1))
-        action c)
-    (catch 'end-flag
-      (while t
-        (setq action
-              (read-key-sequence-vector (format "size[%dx%d]"
-                                                (window-width)
-                                                (window-height))))
-        (setq c (aref action 0))
-        (cond ((= c ?l)
-               (enlarge-window-horizontally dx))
-              ((= c ?h)
-               (shrink-window-horizontally dx))
-              ((= c ?j)
-               (enlarge-window dy))
-              ((= c ?k)
-               (shrink-window dy))
-              ;; otherwise
-              (t
-               (let ((last-command-char (aref action 0))
-                     (command (key-binding action)))
-                 (when command
-                   (call-interactively command)))
-               (message "Quit")
-               (throw 'end-flag t)))))))
