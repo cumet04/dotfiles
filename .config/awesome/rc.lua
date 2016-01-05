fd9c1e94052f35989e3869a77e89736ec59d5bb2
medalhkr <medalhkr@gmail.com>
2016-01-06 02:01:02 +0900

    [change] awesomeのhotkeyを一部変更
    mac上の仮想化にも対応できるように

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -269,12 +269,12 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
-    awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
-    awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
-    awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1)      end),
-    awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)      end),
-    awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
-    awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
+    awful.key({ modkey, "Control" }, "l",     function () awful.tag.incmwfact( 0.05)    end),
+    awful.key({ modkey, "Control" }, "h",     function () awful.tag.incmwfact(-0.05)    end),
+    awful.key({ modkey, "Shift"   }, "a",     function () awful.tag.incnmaster( 1)      end),
+    awful.key({ modkey, "Shift"   }, "x",     function () awful.tag.incnmaster(-1)      end),
+    awful.key({ modkey, "Control" }, "a",     function () awful.tag.incncol( 1)         end),
+    awful.key({ modkey, "Control" }, "x",     function () awful.tag.incncol(-1)         end),
     awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts,  1) end),
 
     awful.key({ modkey, "Control" }, "n", awful.client.restore),
