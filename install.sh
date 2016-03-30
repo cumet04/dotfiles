cdbe35d57d4c1810cfe8b26a1c64a9a4314099b1
medalhkr <medalhkr@gmail.com>
2016-03-30 16:05:26 +0900

    [add] add font-symlink to install.sh

--- install.sh
+++ install.sh
@@ -7,16 +7,21 @@ expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
 rm "$HOME/.bashrc"
 ln -s "$cwd/.bashrc" "$HOME/"
 ln -s "$cwd/.inputrc" "$HOME/"
-ln -s "$cwd/.xprofile" "$HOME/"
+ln -s "$cwd/.xinitrc" "$HOME/"
 ln -s "$cwd/.gitconfig" "$HOME/"
-ln -s "$cwd/.gitignore" "$HOME/"
 ln -s "$cwd/.tmux.conf" "$HOME/"
 ln -s "$cwd/.emacs.d" "$HOME/"
 mkdir "$HOME/.config"
+ln -s "$cwd/.config/IdeaIC2016.1" "$HOME/.config/"
 ln -s "$cwd/.config/nvim" "$HOME/.config/"
 ln -s "$cwd/.config/awesome" "$HOME/.config/"
 ln -s "$cwd/.config/xfce4" "$HOME/.config/"
 ln -s "$cwd/.config/ranger" "$HOME/.config/"
 ln -s "$cwd/.config/powerline-shell.py" "$HOME/.config/"
+ln -s "$cwd/.config/gitignore_global" "$HOME/.config/"
+
+sudo ln -s /etc/fonts/conf.avail/10-unhinted.conf /etc/fonts/conf.d/
+sudo ln -s /etc/fonts/conf.avail/70-no-bitmaps /etc/fonts/conf.d/
+sudo -k
 
 echo "finish install"
