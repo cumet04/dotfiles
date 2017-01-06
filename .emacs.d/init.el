a67e7ee82ae5fdc04b935ab2f1b7961fd9f65457
medalhkr <medalhkr@gmail.com>
2017-01-06 19:21:08 +0900

    [add] emacsのクリップボード共有のオプションを追加

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -61,3 +61,16 @@
                     (process-send-string proc text)
                     (process-send-eof proc)))
         )))
+
+(if (string-match "apple-darwin" system-configuration)
+    (progn
+        (defun copy-from-osx ()
+         (shell-command-to-string "pbpaste"))
+        (defun paste-to-osx (text &optional push)
+         (let ((process-connection-type nil))
+             (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+               (process-send-string proc text)
+               (process-send-eof proc))))
+        (setq interprogram-cut-function 'paste-to-osx)
+        (setq interprogram-paste-function 'copy-from-osx)
+    ))
