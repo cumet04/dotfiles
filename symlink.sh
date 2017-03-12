7f9831ed8fc28d8b4f1ac6879d66342620a425bc
medalhkr <medalhkr@gmail.com>
2017-03-13 00:10:47 +0900

    [fix] 文法エラーしていたのを修正

--- symlink.sh
+++ symlink.sh
@@ -4,7 +4,7 @@
 cwd=`dirname "${0}"`
 expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
 
-rm "$HOME/.bashrc"
+rm "$HOME/.bashrc" 2> /dev/null
 for target in $(echo "
     .emacs.d
     .gitconfig
@@ -13,7 +13,7 @@ for target in $(echo "
     .xinitrc
     .Xmodmap
     .zshrc
-");do ln -s "$cwd/$target" "$HOME/"
+");do ln -sf "$cwd/$target" "$HOME/";done
 
 mkdir "$HOME/.config"
 for target in $(echo "
@@ -22,11 +22,15 @@ for target in $(echo "
     nvim
     systemd
     terminator
-");do ln -s "$cwd.config/$target" "$HOME/.config"
+");do ln -sf "$cwd/.config/$target" "$HOME/.config";done
+
+mkdir $HOME/.config/zsh
+mkdir -p $HOME/.config/inkscape/keys
 
 cp /usr/share/inkscape/keys/adobe-illustrator-cs2.xml $HOME/.config/inkscape/keys/default.xml
 # cp /Applications/Inkscape.app/Contents/Resources/share/inkscape/keys/adobe-illustrator-cs2.xml $HOME/.config/inkscape/keys/default.xml
 
+sudo ln -s /usr/share/git/diff-highlight/diff-highlight /usr/local/bin/
 sudo ln -s /etc/fonts/conf.avail/10-unhinted.conf /etc/fonts/conf.d/
 sudo ln -s /etc/fonts/conf.avail/70-no-bitmaps /etc/fonts/conf.d/
 sudo -k
