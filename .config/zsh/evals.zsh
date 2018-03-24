0dab4d1b31c2ee816b3079c938b3b88727a6e875
Hikaru Inomoto <hikaru.inomoto@jp.oro.com>
2018-03-24 23:23:26 +0900

    pyenv

--- .config/zsh/evals.zsh
+++ .config/zsh/evals.zsh
@@ -42,6 +42,26 @@ nodenv() {
   esac
 }
 
+export PYENV_ROOT="/opt/pyenv"
+# pyenv; eval $(pyenv init - --no-rehash)
+export PATH="/opt/pyenv/shims:${PATH}"
+export PYENV_SHELL=zsh
+source '/opt/pyenv/libexec/../completions/pyenv.zsh'
+pyenv() {
+  local command
+  command="$1"
+  if [ "$#" -gt 0 ]; then
+    shift
+  fi
+
+  case "$command" in
+  rehash|shell)
+    eval "$(pyenv "sh-$command" "$@")";;
+  *)
+    command pyenv "$command" "$@";;
+  esac
+}
+
 # export EXENV_ROOT="/opt/exenv"
 # # exenv; eval $(exenv init - -no-rehash)
 # export PATH="/opt/exenv/shims:${PATH}"
