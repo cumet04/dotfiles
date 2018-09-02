d5de6467000e42d447c5eac4eac7d3954e4aa7a9
medalhkr <medalhkr@gmail.com>
2018-09-02 18:24:51 +0900

    setting for ghq

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,9 @@ alias lt='ls -alht'
 alias cp='cp -i'
 alias mv='mv -i'
 alias vim='nvim'
+alias cdg="cd (git rev-parse --show-toplevel)"
 alias ssh='colored_ssh'
 alias tmux='tmux -f ~/.config/tmux.conf'
 
 set -U GHQ_SELECTOR peco
+set -Ux GOPATH /opt/var/go
