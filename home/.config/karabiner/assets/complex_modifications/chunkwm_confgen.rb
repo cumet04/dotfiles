0f8c5118a18f2746ae3699adc179a3bd9d664499
medalhkr <medalhkr@gmail.com>
2018-10-06 19:09:50 +0900

    refactor chunkwm config generator

--- home/.config/karabiner/assets/complex_modifications/chunkwm_confgen.rb
+++ home/.config/karabiner/assets/complex_modifications/chunkwm_confgen.rb
@@ -1,73 +1,5 @@
 require 'json'
 
-def conf
-    mod1 = "left_control"
-    mod2 = "right_option"
-    {
-        title: 'chunkwm key bindings',
-        rules: [
-            rule("focus application", 
-                hotkey_for_command(
-                    [ mod1 ], "t",
-                    "open /Applications/iTerm.app"
-                ),
-                hotkey_for_command(
-                    [ mod1 ], "b",
-                    "open '/Applications/Google Chrome.app'"
-                ),
-            ),
-            rule("move window", 
-                hotkey_for_command(
-                    [ mod1, mod2 ], "close_bracket", 
-                    "/usr/local/bin/chunkc tiling::window --swap next"
-                ),
-                hotkey_for_command(
-                    [ mod1, mod2 ], "open_bracket", 
-                    "/usr/local/bin/chunkc tiling::window --swap prev"
-                ),
-                *( (1..9).map(&:to_s).map{ |i| 
-                    hotkey_for_command(
-                        [ mod1, mod2 ], i, 
-                        "/usr/local/bin/chunkc tiling::window --send-to-desktop #{i}"
-                    )
-                } )
-            ),
-            rule("focus window", 
-                hotkey_for_command(
-                    [ mod1 ], "close_bracket", 
-                    "/usr/local/bin/chunkc tiling::window --focus next"
-                ),
-                hotkey_for_command(
-                    [ mod1 ], "open_bracket", 
-                    "/usr/local/bin/chunkc tiling::window --focus prev"
-                ),
-            ),
-            rule("resize", 
-                hotkey_for_command(
-                    [ mod1 ], "h", 
-                    "/usr/local/bin/chunkc tiling::window --use-temporary-ratio 0.05 --adjust-window-edge west"
-                ),
-                hotkey_for_command(
-                    [ mod1 ], "l", 
-                    "/usr/local/bin/chunkc tiling::window --use-temporary-ratio 0.05 --adjust-window-edge east"
-                ),
-                hotkey_for_command(
-                    [ mod1 ], "k", 
-                    "/usr/local/bin/chunkc tiling::window --use-temporary-ratio 0.05 --adjust-window-edge north"
-                ),
-                hotkey_for_command(
-                    [ mod1 ], "j", 
-                    "/usr/local/bin/chunkc tiling::window --use-temporary-ratio 0.05 --adjust-window-edge south"
-                ),
-                hotkey_for_command(
-                    [ mod1 ], "f", 
-                    "/usr/local/bin/chunkc tiling::window --toggle fullscreen"
-                ),
-            ),
-        ]
-    }
-end
-
 def rule(desc, *rules)
     {
         description: desc,
@@ -92,5 +24,31 @@ def hotkey_for_command(mod, key, command)
     }
 end
 
+mod1 = "left_control"
+mod2 = "right_option"
+chunk_tile = "/usr/local/bin/chunkc tiling::window"
 
-puts JSON.pretty_generate(conf)
+puts JSON.pretty_generate({title: 'chunkwm key bindings', rules: [
+    rule("focus application",
+        hotkey_for_command( [ mod1 ], "t", "open /Applications/iTerm.app"),
+        hotkey_for_command( [ mod1 ], "b", "open '/Applications/Google Chrome.app'"),
+    ),
+    rule("move window",
+        hotkey_for_command( [ mod1, mod2 ], "close_bracket", "#{chunk_tile} --swap next"),
+        hotkey_for_command( [ mod1, mod2 ], "open_bracket", "#{chunk_tile} --swap prev"),
+        *( (1..9).map(&:to_s).map{ |i|
+            hotkey_for_command( [ mod1, mod2 ], i, "#{chunk_tile} --send-to-desktop #{i}")
+        } )
+    ),
+    rule("focus window",
+        hotkey_for_command( [ mod1 ], "close_bracket", "#{chunk_tile} --focus next"),
+        hotkey_for_command( [ mod1 ], "open_bracket", "#{chunk_tile} --focus prev"),
+    ),
+    rule("resize",
+        hotkey_for_command( [ mod1 ], "h", "#{chunk_tile} --use-temporary-ratio 0.05 --adjust-window-edge west"),
+        hotkey_for_command( [ mod1 ], "l", "#{chunk_tile} --use-temporary-ratio 0.05 --adjust-window-edge east"),
+        hotkey_for_command( [ mod1 ], "k", "#{chunk_tile} --use-temporary-ratio 0.05 --adjust-window-edge north"),
+        hotkey_for_command( [ mod1 ], "j", "#{chunk_tile} --use-temporary-ratio 0.05 --adjust-window-edge south"),
+        hotkey_for_command( [ mod1 ], "f", "#{chunk_tile} --toggle fullscreen"),
+    ),
+]})
