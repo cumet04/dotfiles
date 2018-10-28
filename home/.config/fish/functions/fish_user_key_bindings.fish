a786a3c7c611b872b0f9f80aa6e198d336b1b3f3
medalhkr <medalhkr@gmail.com>
2018-10-28 16:23:33 +0900

    history merge on history-search

--- home/.config/fish/functions/fish_user_key_bindings.fish
+++ home/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,5 @@
 function fish_user_key_bindings
-    bind \cr 'commandline (builtin history | peco --layout=bottom-up);commandline -f repaint'
+    bind \cr history_search
     bind \cg ghq_look
     bind \co walk
     bind \cs 'tig status;commandline -f repaint'
