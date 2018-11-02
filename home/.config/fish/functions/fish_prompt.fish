29d78d064836f2eb6fb40a112e7ebba46491f349
medalhkr <medalhkr@gmail.com>
2018-11-03 00:22:48 +0900

    remove ambiguos character from prompt

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -1,12 +1,10 @@
 function fish_prompt
-    set powerline_color brgreen
-    set text_color black
+    set prompt_color brgreen
 
-    set_color -b $powerline_color
-    set_color $text_color
-    echo -n " "(basename (__tmux_status_pwd))" "
-    set_color -b normal
-    set_color $powerline_color
-    printf '\uE0B0 '
+    set_color $prompt_color -u $prompt_color
+    echo -n (basename (__tmux_status_pwd))
+    set_color -u normal
+    set_color $prompt_color
+    echo -n "> "
     set_color normal
 end
