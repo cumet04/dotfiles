1d5e1ce86c5ff4a012fa6cdac62e51893dfeb293
cumet04 <medalhkr@gmail.com>
2020-04-26 00:12:40 +0900

    remove pip

--- install.sh
+++ install.sh
@@ -2,13 +2,15 @@
 
 set -eu
 
-umask 022
+sudo env DEBIAN_FRONTEND=noninteractive apt-get install -y ansible unzip
 
-sudo env DEBIAN_FRONTEND=noninteractive apt-get install -y python3 python3-pip unzip
-pip3 install ansible awscli
+### install awscli v2
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 
 ### personal files
-~/.local/bin/aws s3 cp s3://${PERSONAL_BUCKET}/personal.tar.gz $HOME/
+aws s3 cp s3://${PERSONAL_BUCKET}/personal.tar.gz $HOME/
 cd $HOME
 tar xzvf ./personal.tar.gz
 find .personal/ -type d | tail -n +2 | sed 's|\.personal/||g' | xargs mkdir -p
@@ -25,4 +27,4 @@ cd /tmp
 curl -L https://github.com/cumet04/dotfiles/archive/$BRANCH.zip -o dotfiles.zip
 unzip dotfiles.zip
 cd dotfiles-$BRANCH/playbook
-~/.local/bin/ansible-playbook -K -e ansible_python_interpreter=/usr/bin/python3 entry.yaml
+ansible-playbook -K entry.yaml
