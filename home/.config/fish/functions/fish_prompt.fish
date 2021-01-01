94dc564c28711288238112b71faf94abab3657cc
Hikaru Inomoto <medalhkr@gmail.com>
2021-01-01 21:15:25 +0900

    remove fish_prompt dependency for shell_script

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -2,7 +2,7 @@ function fish_prompt
     set prompt_color brgreen
 
     set_color $prompt_color -u $prompt_color
-    echo -n (basename (__tmux_status_pwd))
+    echo -n (pwd | sed "s|$HOME|\$HOME|" | xargs basename)
     set_color -u normal
     set_color $prompt_color
     echo -n "> "
