0d230bfe609567bb0488b064c90ab1d12765ba2e
Hikaru Inomoto <medalhkr@gmail.com>
2021-01-24 00:17:14 +0900

    add git command

---
+++ home/.config/fish/functions/newbranch.fish
@@ -0,0 +1,10 @@
+function newbranch
+  set default_branch (git remote -v | head -n 1 | cut -f 2 | \
+    sed 's|https://github.com/\([^. ]*\).*|\1|' | \
+    xargs -I REPO gh api repos/REPO | \
+    jq -r '.default_branch'
+  )
+  git fetch origin --prune
+  git branch $argv[1] origin/$default_branch
+  git checkout $argv[1] 
+end
