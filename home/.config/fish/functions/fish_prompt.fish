9d0ef0ec7f9fe5410ef5ad17ff92b0c9ab103441
cumet04 <medalhkr@gmail.com>
2021-12-19 15:32:06 +0900

    for light theme

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt
-    set prompt_color brgreen
+    set prompt_color green
 
     set_color $prompt_color -u $prompt_color
     echo -n (pwd | sed "s|$HOME|\$HOME|" | xargs basename)
