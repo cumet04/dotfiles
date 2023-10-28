334ee7f02b1e855ad86208f6e77cb316ff3c7d1f
inomoto <medalhkr@gmail.com>
2023-10-28 13:38:07 +0900

    どうせgithubしかないので、リストに冗長なgithub.comを出さないようにする

--- home/.config/fish/functions/repo_look.fish
+++ home/.config/fish/functions/repo_look.fish
@@ -3,12 +3,13 @@ function repo_look
   set selected (
     find $root -maxdepth 3 -mindepth 3 -type d,l |
     sed "s|$root/||g" |
+    sed "s|github\.com/||g" |
     sort |
     peco --on-cancel error
   )
   test $status -eq 0; or return $status;
 
-  set target $root/$selected
+  set target $root/github.com/$selected
   if test -d $target
     builtin cd $target
     git s
