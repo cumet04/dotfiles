4d059d0f9685e354d47148f8f4a257940750e4dd
inomoto <medalhkr@gmail.com>
2023-12-31 13:42:02 +0900

    err statusを表示

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -1,10 +1,18 @@
 function fish_prompt
+    set prev_status $status
+
     set prompt_color green
+    set prompt_color_err red
 
     set_color $prompt_color -u $prompt_color
     echo -n (pwd | sed "s|$HOME|\$HOME|" | xargs basename)
     set_color -u normal
-    set_color $prompt_color
+
+    if test "$prev_status" = 0
+        set_color $prompt_color
+    else
+        set_color $prompt_color_err
+    end
     echo -n "> "
     set_color normal
 
